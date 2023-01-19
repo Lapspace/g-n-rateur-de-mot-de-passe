ce2bbbc594f03acf9228dd482ebcae837e175cc8
Lapspace <93282953+Lapspace@users.noreply.github.com>
2023-01-19 20:10:24 +0100

    Add files via upload

---
+++ générateur de mot de passe chifre.py
@@ -0,0 +1,26 @@
+import random
+
+#caractere
+
+lettre = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+nombre = "0123456789"
+syboles = "@#$&*/\?"
+
+caractere = nombre
+caractere_no_syboles = lettre + lettre.lower() + nombre
+
+while True:
+    longueurmdp = int(input("Entrez la longueur du mot de passe :"))
+    nombredemdp = int(input("Entrez le nombre de mot de passe a afficher :"))
+    caractere_speciaux_ask = str(input("Faut il mettre des caractere speciaux dans le mot de passe ? :"))
+    if caractere_speciaux_ask == 'non' :
+        for i in range(0,nombredemdp):
+            mdp = ""
+            for i in range(0,longueurmdp):
+                cmdp = random.choice(caractere)
+                mdp =mdp + cmdp
+            print('votre mot de passe est :', mdp)
+
+
+    elif caractere_speciaux_ask != 'oui' or 'non':
+        print (ERREUR)
